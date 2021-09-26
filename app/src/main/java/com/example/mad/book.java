7021839a2ebba9b3ee08b802dabf4408ac09e2c3
IT20114312 <it20114312@my.sliit.lk>
2021-09-26 15:01:28 +0530

    first try

--- app/src/main/java/com/example/mad/book.java
+++ app/src/main/java/com/example/mad/book.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class book extends AppCompatActivity {
-
+ String tool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
